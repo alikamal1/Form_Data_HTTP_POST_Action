18aa82f6879af8e09e725b257d4616cb0f44df25
Ali Kamal <alikamaltaki@gmail.com>
2021-11-29 10:45:37 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,23 +5,31 @@ const axios = require('axios');
 
 async function run() {
   try {
+    console.log(url)
+    console.log(headers)
+    console.log(name)
+    console.log(path)
+  
     const url = core.getInput('url')
-    const headers = core.getInput('headers')
+    const headers = JSON.parse(core.getInput('headers') || [])  
+    console.log(headers)
     const name = core.getInput('name')
     const path = core.getInput('file')
     core.info(`Connecting to endpoint (${url}) ...`)
 
+    
     const form = new FormData();
+
     form.append(name, fs.createReadStream(path));
 
     const response = await axios({
       method: 'POST',
       url: url,
-      headers: { 'Content-Type': 'multipart/form-data', ...[headers]},
+      headers: { 'Content-Type': 'multipart/form-data', ...[headers] },
       data: form,
     })
 
-    core.setOutput('Response:', response.data);
+    core.setOutput('response', { 'statusCode': response.statusCode, 'data': response.data });
   } catch (error) {
     core.setFailed(error.message);
   }
