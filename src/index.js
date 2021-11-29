364c15862a24cae0f4d8f0f5e4d2f9dd2d70d42e
Ali Kamal <alikamaltaki@gmail.com>
2021-11-29 10:47:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ const axios = require('axios');
 
 async function run() {
   try {
+
+  
+    const url = core.getInput('url')
+    const headers = JSON.parse(core.getInput('headers') || [])  
+    const name = core.getInput('name')
+    const path = core.getInput('file')
     console.log(url)
     console.log(headers)
     console.log(name)
     console.log(path)
-  
-    const url = core.getInput('url')
-    const headers = JSON.parse(core.getInput('headers') || [])  
-    console.log(headers)
-    const name = core.getInput('name')
-    const path = core.getInput('file')
     core.info(`Connecting to endpoint (${url}) ...`)
 
     
